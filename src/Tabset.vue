c0910a04a5c47f2de48cf844c5abcb23d5f81ec6
英布 <chaoren1641@gmail.com>
2016-01-19 13:23:50 +0800

    add files from github

---
+++ src/Tabset.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!-- Nav tabs -->
+     <ul class="nav nav-tabs" role="tablist">
+            <li
+                v-for="r in renderData"
+                v-bind:class="{
+                  'active': ($index === activeIndex),
+                  'disabled': r.disabled
+                }"
+                @click.prevent="handleTabListClick($index, r)"
+                :disabled="r.disabled"
+            >
+                <a href="#">{{r.header}}</a>
+            </li>
+     </ul>
+
+     <!-- Tab panes -->
+     <div class="tab-content" v-el:tabContent>
+        <slot></slot>
+     </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      effect: {
+        type: String,
+        default: 'fadein'
+      }
+    },
+    data() {
+      return {
+        renderData: [],
+        activeIndex: 0
+      }
+    },
+    methods: {
+        handleTabListClick(index, el) {
+            if (!el.disabled) this.activeIndex = index
+        }
+    }
+  }
+</script>
+
+<style scoped>
+  .nav-tabs {
+    margin-bottom: 15px
+  }
+</style>
---
+++ src/Tooltip.vue
@@ -0,0 +1,68 @@
+<template>
+  <span v-el:trigger>
+    <slot>
+    </slot>
+  </span>
+  <div class="tooltip"
+    v-bind:class="{
+    'top':    placement === 'top',
+    'left':   placement === 'left',
+    'right':  placement === 'right',
+    'bottom': placement === 'bottom'
+    }"
+    v-el:popover
+    v-show="show"
+    :transition="effect"
+    role="tooltip">
+    <div class="tooltip-arrow"></div>
+    <div class="tooltip-inner">
+      {{{content}}}
+    </div>
+  </div>
+</template>
+
+<script>
+import PopoverMixin from './popoverMixins.js'
+  export default {
+    mixins: [PopoverMixin],
+    props: {
+      trigger: {
+        type: String,
+        default: 'hover'
+      },
+      effect: {
+        type: String,
+        default: 'scale'
+      }
+    }
+  }
+</script>
+
+<style>
+  .tooltip {
+    opacity: .9
+  }
+.fadein-enter {
+  animation:fadein-in 0.3s ease-in;
+}
+.fadein-leave {
+  animation:fadein-out 0.3s ease-out;
+}
+@keyframes fadein-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fadein-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+</style>
