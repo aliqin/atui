5596d22313a5350eeebaee7639de08265482d72d
英布 <chris.chencq@alibaba-inc.com>
2016-01-21 10:35:29 +0800

    同步demo

--- depoly.js
+++ depoly.js
@@ -3,7 +3,7 @@ var path = require('path')
 ghpages.publish(path.join(__dirname, './'), {
   branch: 'demo',
   message: '同步demo',
-  // add:true,
+  add:true,
   src:'!(node_modules)',
   logger: function(message) {
     console.log(message);
