b0d56ec82bcd2d9838b78ee7b272fdc0ed4e7840
若欢 <ruohuan.zhh@alibaba-inc.com>
2016-01-28 17:24:52 +0800

    UItest开启watch

--- test/Alert.test.js
+++ test/Alert.test.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import Alert from '../src/components/Alert.vue'
 
 describe('Alert.vue', () => {
-  it('Alert组件位置应该在右上角', () => {
+  it('Alert组件的显示内容是否正确', () => {
     const vm = new Vue({
+      data() {
+        return {
+          show: true
+        }
+      },
       template: '<div>'+
-                  '<alert show="true"'+
-                    'duration="3000"'+
+                  '<alert :show.sync="show"'+
+                    ':duration="3000"'+
                     'type="success"'+
                     'width="400px"'+
                     'placement="top-right"'+
@@ -18,25 +23,8 @@ describe('Alert.vue', () => {
                 '</div>',
       components: { Alert }
     }).$mount()
-    expect(vm.$el.querySelector('.alert').className).to.contain('top-right')
-  })
-  it('Alert组件位置应该在顶部', () => {
-    const vm = new Vue({
-      template: '<div>'+
-                  '<alert show="true"'+
-                    'duration="3000"'+
-                    'type="success"'+
-                    'width="400px"'+
-                    'placement="top"'+
-                    'dismissable>'+
-                    '<span class="icon-ok-circled alert-icon-float-left"></span>'+
-                    '<strong>我是标题!</strong>'+
-                    '<p>我是具体内容.</p>'+
-                  '</alert>'+
-                '</div>',
-      components: { Alert }
-    }).$mount()
-    expect(vm.$el.querySelector('.alert').className).to.contain('top')
+    expect(vm.$el.querySelector('.alert').textContent).to.contain('我是标题!')
+    expect(vm.$el.querySelector('.alert').textContent).to.contain('我是具体内容.')
   })
 })
 
