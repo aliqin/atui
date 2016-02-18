c9bf46103aa220e2d39d269451e5f1a9759fb92e
英布 <chris.chencq@alibaba-inc.com>
2016-02-18 10:34:37 +0800

    table组件添加

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -18,7 +18,7 @@
         </tr>
     </thead>
     <tbody>
-        <tr v-show="!dataSrouce.length"><td colspan="10000" class="vue-table-empty">没有任何数据</td></tr>
+        <tr v-show="!dataSrouce.length"><td colspan="10000" style="text-align: center;" class="vue-table-empty">没有任何数据</td></tr>
         <tr v-for="
             (rowIndex, entry) in dataSrouce
             | filterBy filterKey
@@ -38,7 +38,200 @@
     </tbody>
 </table>
 </template>
-<script>
+
+<style lang="less">
+.vue-table {
+  width:100%;
+  border-collapse: collapse;
+
+  thead {
+    background-color: #F7F7F7;
+    border-bottom: 1px solid #D1D1D1;
+
+    tr:hover {
+      background-color: #F7F7F7;
+    }
+
+    .multi-col {
+      position: relative;
+      &:hover {
+        cursor: pointer;
+
+        ul {
+          display: block;
+        }
+      }
+
+      ul {
+        display: none;
+        position: absolute;
+        width: 100%;
+        z-index: 10;
+
+        li {
+            background-color: #fff;
+        }
+
+        a {
+            display: block;
+            height: 32px;
+            padding: 7px 8px;
+            text-decoration: none;
+            color: #333;
+            font-size: 12px;
+            line-height: 20px;
+
+        &:hover {
+             cursor: pointer;
+             background-color: #f5f5f5;
+         }
+        }
+      }
+    }
+  }
+
+  tbody,tfoot {
+    background-color: #FFF;
+  }
+
+  th,td {
+    //.text-overflow;
+    text-align:left;
+    vertical-align: middle;
+  }
+
+  th {
+    // .user-select(none);
+
+    position:relative;
+    padding:12px 15px;
+    color:#333;
+  }
+
+  tr:hover {
+    background-color: #FEFEFE;
+  }
+
+  tr.none {
+    td {
+      text-align: center;
+    }
+
+    &:hover {
+      background-color: #FFF;
+    }
+  }
+
+  tfoot {
+    tr:hover {
+      background-color: inherit;
+    }
+  }
+
+  td {
+    //.text-overflow;
+
+    position: relative;
+    padding: 15px;
+    border-bottom: 1px solid #F0F0F0;
+    color:#555;
+  }
+
+  a.btn-op {
+      margin-right: 10px;
+      color: #0083cd;
+      text-decoration: none;
+      cursor: pointer;
+      font-size: 12px;
+      white-space: nowrap;
+
+     &:active {
+         color: #0083cd;;
+     }
+  }
+
+  .filter,
+  .sort {
+    cursor: pointer;
+    overflow: inherit;
+
+    img {
+      margin-left:3px;
+      vertical-align: middle;
+    }
+  }
+
+  .sort {
+    .icon-down,
+    .icon-up {
+      display: none;
+    }
+
+    &.down {
+      .icon-down {
+        display: inline-block;
+      }
+      .icon-up {
+        display: none;
+      }
+    }
+
+    &.up {
+      .icon-down {
+        display: none;
+      }
+      .icon-up {
+        display: inline-block;
+      }
+    }
+  }
+
+  .filter-layer {
+    display: none;
+    position: absolute;
+    left:0;
+    top:37px;
+    box-shadow: 2px 2px 2px #EEE;
+    border: 1px solid #D1D1D1;
+    z-index:1;
+    background-color: #FFF;
+
+    span {
+      //.text-overflow;
+
+      display: block;
+      width:100%;
+      text-align: left;
+      padding:5px 10px;
+
+      &:hover {
+        background-color: #FFF9F3;
+      }
+
+      &.on {
+        background-color: #FF6200;
+        color:#FFF;
+      }
+    }
+  }
+
+  .operator {
+    margin-right:5px;
+  }
+
+  .pagination {
+    float:right;
+    margin-top:0;
+  }
+}
+.vue-dataTable-loading {
+  position: absolute;
+  background:url(//img.alicdn.com/tps/i1/T1cKm3XkRpXXXXXXXX-48-48.gif) no-repeat center center;
+}
+
+</style>
+
+<script type="text/babel">
 export default{
     props: {
         dataSrouce: Array,
