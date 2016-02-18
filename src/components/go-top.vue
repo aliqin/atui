0d2bcf9cd8bca02cbcf0d0cb3c851be01a7d8aa8
英布 <chaoren1641@gmail.com>
2016-02-18 10:38:14 +0800

    添加go-top、toast、loading组件

---
+++ src/components/go-top.vue
@@ -0,0 +1,54 @@
+<template lang="jade">
+.go-top.iconfont(:style="{width:width,height:height,visibility:visibility}",@click="toTop",v-bind:title="tipText") &#xe605;
+</template>
+
+<style lang="less">
+.go-top{
+    position: fixed;
+    cursor: pointer;
+    bottom:40px;
+    right:10px;
+    width:40px;
+    height:40px;
+    border:1px solid #ddd;
+    border-radius: 2px;
+    text-align: center;
+    &.iconfont{
+        font-size: 28px;
+    }
+}
+
+</style>
+
+<script>
+    export default {
+        props: {
+            width:String,
+            height:String,
+            tipText:{
+                type:String,
+                default:'返回顶部'
+            }
+        },
+        data(){
+            return {
+                visibility:'hidden'
+            }
+        },
+        methods: {
+            toTop() {
+                document.body.scrollTop = 0;
+            }
+        },
+        created() {
+            var self = this;
+            window.addEventListener('scroll',(e)=>{
+                if(document.body.scrollTop > 200) {
+                    self.visibility = 'visible';
+                } else {
+                    self.visibility = 'hidden';
+                }
+            })
+        }
+    }
+</script>
