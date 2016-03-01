11613b7199050456da5064e9cdf50d68be0a3c55
英布 <chaoren1641@gmail.com>
2016-03-01 11:24:41 +0800

    按钮文档

--- docs/example/formDocs.vue
+++ docs/example/formDocs.vue
@@ -1,36 +1,4 @@
 <template>
-  <div class="bs-docs-section" id="button">
-    <h3 class="page-header"><a href="#button" class="anchor">button 按钮</a></h3>
-    <div class="bs-example">
-      <form>
-      <!-- Standard button -->
-    <button type="button" class="btn btn-default">（默认样式）Default</button>
-
-    <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-    <button type="button" class="btn btn-primary">（首选项）Primary</button>
-
-    <!-- Indicates a successful or positive action -->
-    <button type="button" class="btn btn-success">（成功）Success</button>
-
-    <!-- Contextual button for informational alert messages -->
-    <button type="button" class="btn btn-info">（一般信息）Info</button>
-
-    <!-- Indicates caution should be taken with this action -->
-    <button type="button" class="btn btn-warning">（警告）Warning</button>
-
-    <!-- Indicates a dangerous or potentially negative action -->
-    <button type="button" class="btn btn-danger">（危险）Danger</button>
-
-    <!-- Deemphasize a button by making it look like a link while maintaining button behavior -->
-    <button type="button" class="btn btn-link">（链接）Link</button>
-      </form>
-    </div>
-    <pre><code class="language-markup"><script type="language-mark-up">
-    </script></code></pre>
-    </script>
-    </code>
-  </div>
-
 
   <div class="bs-docs-section" id="form">
     <h3 class="page-header"><a href="#form" class="anchor">form 表单 </a></h3>
