1c09df3b451e2418e3e8d377c7108a3df3c9ef8e
wangtianlun <wb-wangtianlun@alibaba-inc.com>
2016-03-10 12:34:29 +0800

    添加badge组件

---
+++ src/components/Badge.vue
@@ -0,0 +1,30 @@
+<template>
+  <span v-if="dot" class="badge">
+    <slot></slot>
+    <sup class="badge-dot"></sup>
+  </span>
+  <span v-else class="badge">
+    <slot></slot>
+    <sup v-if='count' class="badge-count">
+      {{count | short}}
+    </sup>
+  </span>
+</template>
+
+<script >
+  export default {
+    props: {
+      count: {
+        type: Number
+      },
+      dot: {
+        type: Boolean
+      }
+    },
+    filters: {
+      short: (value) => {
+        return value >= 100 ? '99+' : value;
+      }
+    }
+  }
+</script>
