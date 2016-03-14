b3c5c7c2aeac6069fd87cc282257cebc2936d6fe
wangtianlun <wb-wangtianlun@alibaba-inc.com>
2016-03-14 14:20:24 +0800

    添加switch开关组件

---
+++ docs/example/switchDocs.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="bs-docs-section" id="switch">
+    <h3 class="page-header"><a href="#switch" class="anchor">Switch 开关</a></h3>  
+    <div class="bs-example">
+      <h4 class="example-title"></h4>
+      
+      <h4 class="example-title"></h4>
+      
+      <h4 class="example-title"></h4>
+    </div>
+<pre>
+<code class="language-markup"><script type="language-mark-up">
+
+</script></code></pre>
+  </div>
+  <h2>Options</h2>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Default</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td><code></code></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><code></code></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  import Switch from 'src/components/Switch.vue'
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      Switch
+    }
+  }
+</script>
+
+<style lang="less">
+  .example-title {
+    margin: 20px 0;
+  }
+</style>
