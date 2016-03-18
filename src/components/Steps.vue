c8258f7b6b363ab5bf353df916a3d09839c6b8b6
wangtianlun <wb-wangtianlun@alibaba-inc.com>
2016-03-18 18:33:40 +0800

    添加步骤条组件

---
+++ src/components/Steps.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="steps">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		ready () {
+
+		}
+	}
+</script>
+
