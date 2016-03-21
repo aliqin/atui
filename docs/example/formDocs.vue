3f26a0c0b6b15283c0b649faa0e0c565ca2e4fe0
若欢 <ruohuan.zhh@alibaba-inc.com>
2016-03-21 11:40:45 +0800

    textarea

--- docs/example/formDocs.vue
+++ docs/example/formDocs.vue
@@ -4,7 +4,7 @@
     <h3 class="page-header"><a href="#form" class="anchor">form 表单 </a></h3>
     <div class="bs-example">
       <form>
-        <div class="form-group">
+        <!-- <div class="form-group">
           <label for="exampleInputEmail1">邮件地址</label>
           <input type="email" class="form-control" id="exampleInputEmail1" placeholder="请输入邮箱">
         </div>
@@ -16,16 +16,28 @@
           <label for="exampleInputFile">文件上传</label>
           <input type="file" id="exampleInputFile">
           <p class="help-block">block-level提示文字</p>
-        </div>
+        </div> -->
         <div class="checkbox">
-          <label>
-            <input type="checkbox"> 选中框
-          </label>
+          <h4>checkbox</h4>
+          <checkbox :checked.sync="false"></checkbox>
+          <br>
+          <checkbox :checked.sync="true"></checkbox>
+          <br>
+          <checkbox :checked.sync="false" :disabled.sync="true"></checkbox>
+          <br>
+          <checkbox :checked.sync="true" :disabled.sync="true"></checkbox>
         </div>
-        <button type="submit" class="btn btn-default">提交</button>
+        <button type="submit" class="btn">提交</button>
       </form>
     </div>
     <pre><code class="language-markup"><script type="language-mark-up">
+checkbox复选框调用方法：
+1、简介型 <checkbox></checkbox>
+可正常选中或取消选中
+2、默认选中或取消选中 <checkbox :checked.sync="true"></checkbox>
+3、被禁用 <checkbox :checked.sync="false" :disabled.sync="true"></checkbox>
+4、已选中的，但被禁用了 <checkbox :checked.sync="true" :disabled.sync="true"></checkbox>
+
 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">邮件地址</label>
@@ -52,5 +64,21 @@
     </code>
   </div>
 </template>
+
+
+<script>
+  import Checkbox from 'src/components/checkbox.vue'
+
+  export default {
+    components: {
+      checkbox: Checkbox
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
 
 
