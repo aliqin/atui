b17307c2b968ca90b6c6184a4e166a2be6c989c5
wangtianlun <wb-wangtianlun@alibaba-inc.com>
2016-03-21 18:22:45 +0800

    添加面包屑组件

---
+++ docs/example/breadcrumbDocs.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="bs-docs-section" id="breadcrumb">
+    <h3 class="page-header"><a href="#breadcrumb" class="anchor">Breadcrumb 面包屑</a></h3>  
+    <div class="bs-example">
+     
+    </div>
+<pre>
+<code class="language-markup"><script type="language-mark-up">
+
+
+</script></code></pre>
+  </div>
+  <h2>Options</h2>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Default</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        
+      </tr>
+      <tr>
+        
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  import breadcrumb from 'src/components/Breadcrumb.vue'
+  import breadcrumbItem from 'src/components/BreadcrumbItem.vue'
+
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    components: {
+      breadcrumb,
+      breadcrumbItem
+    }
+  }
+</script>
+
+<style lang="less">
+  .badge-count-example, .badge-dot-example {
+    width: 42px;
+    height: 42px;
+    background-color: #eee;
+    display: inline-block;
+    border-radius: 6px;
+  }
+  .example-title {
+    margin: 20px 0;
+  }
+</style>
