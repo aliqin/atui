a5447d88f47646b2d40a4a3f436e1288b6cac2bc
英布 <chaoren1641@gmail.com>
2016-03-28 10:43:55 +0800

    重新打包上传

--- src/index.js
+++ src/index.js
@@ -3,44 +3,115 @@ require('../src/styles/themes/tmallwt.less')
 require('../src/styles/themes/alitx.less')
 
 // 表单类
-// export {Button} from './components/Button.vue'
-// export {ButtonsDocs} from './components/Buttons.vue'
-// export {Input} from './components/Input.vue'
-export {Uploader} from './components/Uploader.vue'
-// export {Form} from './components/Form.vue'
-export {Switch} from './components/Switch.vue'
-export {Dropdown} from './components/Dropdown.vue'
-export {Select} from './components/Select.vue'
-export {Datepicker} from './components/Datepicker.vue'
-// export progressbar} from './components/progressbar.vue'
-export {Typeahead} from './components/Typeahead.vue'
+// import Button from './components/Button.vue'
+// import ButtonsDocs from './components/Buttons.vue'
+// // import Input from './components/Input.vue'
+// import Uploader from './components/Uploader.vue'
+// // import Form from './components/Form.vue'
+// import Switch from './components/Switch.vue'
+// import Dropdown from './components/Dropdown.vue'
+// import Select from './components/Select.vue'
+// import Datepicker from './components/Datepicker.vue'
+// // import rogressbar from './components/progressbar.vue'
+// import Typeahead from './components/Typeahead.vue'
+
+
+// // 展示类
+// import Alert from './components/Alert.vue'
+// import Modal from './components/Modal.vue'
+// import Table from './components/Table.vue'
+// import Badge from './components/Badge.vue'
+// import Carousel from './components/Carousel.vue'
+// import Affix from './components/Affix.vue'
+// // import Container from './components/Container.vue'
+// import Spin from './components/Spin.vue'
+// import Textarea from './components/Textarea.vue'
+// import Tag from './components/Tag.vue'
+// import Tooltip from './components/Tooltip.vue'
+// import Accordion from './components/Accordion.vue'
+
+
+// // 导航类
+// // import ffix from './components/affix.vue'
+// // import side from './components/aside.vue'
+// import Popover from './components/Popover.vue'
+// import Tabs from './components/Tabset/'
+// import Steps from './components/Steps/index'
+// import Menu from './components/Menu/index'
+// import Breadcrumb from './components/Breadcrumb.vue'
+// import Pagination from './components/Pagination/'
+
+
+// // filters
+// import phoneNumber from './filters/phoneNumber'
+
+
+// import Button from './components/Button.vue'
+// import ButtonsDocs from './components/Buttons.vue'
+// import Input from './components/Input.vue'
+import Uploader from './components/Uploader.vue'
+// import Form from './components/Form.vue'
+import Switch from './components/Switch.vue'
+import Dropdown from './components/Dropdown.vue'
+import Select from './components/Select.vue'
+import Datepicker from './components/Datepicker.vue'
+// import rogressbar from './components/progressbar.vue'
+import Typeahead from './components/Typeahead.vue'
 
 
 // 展示类
-export {Alert} from './components/Alert.vue'
-export {Modal} from './components/Modal.vue'
-export {Table} from './components/Table.vue'
-export {Badge} from './components/Badge.vue'
-export {Carousel} from './components/Carousel.vue'
-export {Affix} from './components/Affix.vue'
-// export {Container} from './components/Container.vue'
-export {Spin} from './components/Spin.vue'
-export {Textarea} from './components/Textarea.vue'
-export {Tag} from './components/Tag.vue'
-export {Tooltip} from './components/Tooltip.vue'
-export {Accordion} from './components/Accordion.vue'
+import Alert from './components/Alert.vue'
+import Modal from './components/Modal.vue'
+import Table from './components/Table.vue'
+import Badge from './components/Badge.vue'
+import Carousel from './components/Carousel.vue'
+import Affix from './components/Affix.vue'
+// import Container from './components/Container.vue'
+import Spin from './components/Spin.vue'
+import Textarea from './components/Textarea.vue'
+import Tag from './components/Tag.vue'
+import Tooltip from './components/Tooltip.vue'
+import Accordion from './components/Accordion.vue'
 
 
 // 导航类
-// export affix} from './components/affix.vue'
-// export aside} from './components/aside.vue'
-export {Popover} from './components/Popover.vue'
-export {Tabs} from './components/Tabset/'
-export {Steps} from './components/Steps/index'
-export {Menu} from './components/Menu/index'
-export {Breadcrumb} from './components/Breadcrumb.vue'
-export {Pagination} from './components/Pagination/'
+// import ffix from './components/affix.vue'
+// import side from './components/aside.vue'
+import Popover from './components/Popover.vue'
+import Tabs from './components/Tabset/'
+import Steps from './components/Steps/index'
+import Menu from './components/Menu/index'
+import Breadcrumb from './components/Breadcrumb.vue'
+import Pagination from './components/Pagination/'
 
 
 // filters
-export {phoneNumber} from './filters/phoneNumber'
+import phoneNumber from './filters/phoneNumber'
+
+const VueStrap = {
+  Uploader,
+  Switch,
+  Dropdown,
+  Select,
+  Datepicker,
+  Typeahead,
+  Alert,
+  Modal,
+  Table,
+  Badge,
+  Carousel,
+  Affix,
+  Spin,
+  Textarea,
+  Tag,
+  Tooltip,
+  Accordion,
+  Popover,
+  Tabs,
+  Steps,
+  Menu,
+  Breadcrumb,
+  Pagination
+}
+
+module.imports= VueComponent
