1fdf8d03d7eb96e332f578abb5d4b2f9fa97d2b3
英布 <chris.chencq@alibaba-inc.com>
2016-03-31 20:53:13 +0800

    table组件修改

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('src/styles/themes/alidayu.less')
-require('src/styles/themes/tmallwt.less')
-require('src/styles/themes/alitx.less')
+// require('src/styles/themes/tmallwt.less')
+// require('src/styles/themes/alitx.less')
 
 
 
