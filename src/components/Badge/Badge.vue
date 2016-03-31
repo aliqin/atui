e8a5a2ce843762e493f3fe178204b75cb223ef77
wangtianlun <wb-wangtianlun@alibaba-inc.com>
2016-03-31 19:37:20 +0800

    更新badge文档

--- src/components/Badge/Badge.vue
+++ src/components/Badge/Badge.vue
@@ -5,7 +5,7 @@
   </span>
   <span v-else class="badge">
     <slot></slot>
-    <sup v-if='show && count && count >=0' class="badge-count">
+    <sup v-if='count > 0' class="badge-count">
       {{count | short}}
     </sup>
   </span>
@@ -20,15 +20,11 @@
       dot: {
         type: Boolean,
         default: false
-      },
-      show: {
-        type: Boolean,
-        default: false
       }
     },
     filters: {
       short: (value) => {
-        return value >= 100 ? '99+' : value;
+        return value >= 100 ? '99+' : value
       }
     }
   }
