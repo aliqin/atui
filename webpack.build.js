f0dababc4f46c44bd0f06df8db9d9698ae7c57d8
英布 <chris.chencq@alibaba-inc.com>
2016-04-04 19:33:42 +0800

    Merge branch 'daily/0.0.1' of gitlab.alibaba-inc.com:aliqin/vue-component into daily/0.0.1
    # Conflicts:
    #	src/widget/index.js
    #	webpack.build.js

--- webpack.build.js
+++ webpack.build.js
@@ -9,53 +9,30 @@ config.entry = {
 config.output = {
     path: './dist',
     filename: '[name].js',
-    chunkFilename: "[id].js",
     library: '[name]',
     libraryTarget: 'umd'
 }
 config.plugins = (config.plugins || []).concat([
-    // new webpack.ProvidePlugin({
-    //     Vue: 'vue',
-    // }),
-    // new webpack.ProvidePlugin({
-    //     VueComponent: 'vue-component'
-    // }),
-    // new webpack.ProvidePlugin({
-    //     VueWidget: 'vue-widget'
-    // }),
     new webpack.optimize.CommonsChunkPlugin({
         name: 'vue',
         chunks: 'Vue',
         filename:'vue.js'
     }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'vue-component',
-        chunks: 'VueComponent',
-        filename:'vue-component.js'
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'vue-widget',
-        chunks: 'VueWidget',
-        filename: 'vue-widget.js'
-    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     name: 'vue-component',
+    //     chunks: 'VueComponent',
+    //     filename:'vue-component.js'
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     name: 'vue-widget',
+    //     chunks: 'VueWidget',
+    //     filename: 'vue-widget.js'
+    // }),
     new webpack.DefinePlugin({
         'process.env': {
             NODE_ENV: 'production'
         }
     }),
-    // new CommonsChunkPlugin({
-    //   name: 'commons',
-    //   // (the commons chunk name)
-
-    //   filename: 'commons.js',
-    //   // (the filename of the commons chunk)
-
-    //   // minChunks: 3,
-    //   // (Modules must be shared between 3 entries)
-
-    //   // chunks: ['pageA', 'pageB'],
-    //   // (Only use these entries)
-    // })
 
     new webpack.optimize.UglifyJsPlugin({
         compress: {
