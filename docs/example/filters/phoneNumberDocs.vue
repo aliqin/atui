fb4743d28346a2d99811fb67ad0cc40f1d030bfe
英布 <chris.chencq@alibaba-inc.com>
2016-04-14 14:50:11 +0800

    docs update

--- docs/example/filters/phoneNumberDocs.vue
+++ docs/example/filters/phoneNumberDocs.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="bs-docs-section" id="phoneNumber">
-    <h3 class="page-header"><a href="#phoneNumber" class="anchor">手机号码格式化</a></h3>
+  <div class="bs-docs-section" id="phonenumber">
+    <h3 class="page-header"><a href="#phonenumber" class="anchor">手机号码格式化 phonenumber</a></h3>
     <div class="bs-example">
     <input type="text" v-model="number" />
-    <div>{{number | phone-number}}</div>
+    <div>{{number | phoneNumber}}</div>
     </div>
-
 <pre><code class="language-markup"><script type="language-mark-up">
-
+<input type="text" v-model="number" />
+<div>{{number | phoneNumber}}</div>
 </script></code></pre>
   </div>
 </template>
 <script>
-  import phoneNumber from 'src/filters/phoneNumber.js'
+  import 'src/filters/'
   export default {
     data() {
       return {
