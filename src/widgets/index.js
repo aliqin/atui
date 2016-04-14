7fdbf9b3d19bab0dd1b575f11ed051b7fa941b0d
英布 <chris.chencq@alibaba-inc.com>
2016-04-14 09:39:57 +0800

    change widgets path

---
+++ src/widgets/index.js
@@ -0,0 +1,10 @@
+import AddressSelect from './AddressSelect/'
+
+const VueWidget = {
+  AddressSelect
+}
+// require.ensure([], function(require) {
+//   VueWidget.AddressSelect = require('./AddressSelect/')
+//   console.log(VueWidget.AddressSelect)
+// })
+module.exports = VueWidget
