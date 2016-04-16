893e78d48118a471f14f8a8cf861627fd2f3dd87
英布 <chris.chencq@alibaba-inc.com>
2016-04-16 22:02:32 +0800

    日历增加rangePicker文件，样式代码修改

---
+++ src/components/DatePicker/RangePicker.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="range-picker">
+  <input type="" placeholder="开始日期" @click="triggerStart" / >
+    <span class="range-picker-separator"> ~ </span>
+  <input type="text" placeholder="结束日期" @click="triggerEnd">
+  <div class="range-picker-content">
+    <date-picker v-show="showEndPicker || showStartPicker"></date-picker>
+  </div>
+</div>
+</template>
+<script>
+  import DatePicker from './DatePicker.vue'
+  import coerceBoolean from '../utils/coerceBoolean.js'
+  export default {
+    props:{
+      trigger:{
+        type: String,
+        default: 'click'
+      },
+      open:{
+        type: Boolean,
+        coerce: coerceBoolean,
+        default: false
+      }
+    },
+    data() {
+      return {
+        showStartPicker:false,
+        showEndPicker:false
+      }
+    },
+    methods: {
+      triggerStart() {
+        this.showStartPicker = !this.showStartPicker
+      },
+      triggerEnd() {
+        this.showEndPicker = !this.showEndPicker
+      }
+    }
+  }
+</script>
--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,3 +1,6 @@
 import DatePicker from './DatePicker.vue'
+import RangePicker from './RangePicker.vue'
+
+DatePicker.RangePicker = RangePicker
 
 export default DatePicker
