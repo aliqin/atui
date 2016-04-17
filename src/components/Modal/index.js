261081e004111810a2ebb2a219f8495b2a9e8f24
英布 <chris.chencq@alibaba-inc.com>
2016-04-17 23:20:50 +0800

    select 支持tag 75%

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,37 +1,59 @@
 import Modal from './Modal.vue'
-
-Modal.confirm = function(parmas) {
-  let title = parmas.title
-  let content = parmas.content
-  let onOk = parmas.onOk
-  if(!document.getElementsByClassName('atui-modal').length) {
-      let box = document.createElement('div')
-      box.className = 'atui-modal'
-      document.body.appendChild(box)
+import Icon from '../Icon'
+let iconConfig = {
+  'info':{
+    icon:'info',
+    color:'#30b2f2'
+  },
+  'success':{
+    icon:'success',
+    color:'#1dbf38'
+  },
+  'error':{
+    icon:'error',
+    color:'#ff5959'
+  },
+  'confirm':{
+    icon:'help',
+    color:'#b366ff'
+  }
+}
+;['info', 'success', 'error', 'confirm'].forEach((type) => {
+  Modal[type] = function(parmas) {
+    let title = parmas.title
+    let content = parmas.content
+    let onOk = parmas.onOk
+    if (!document.getElementsByClassName('atui-modal').length) {
+        let box = document.createElement('div')
+        box.className = 'atui-modal'
+        document.body.appendChild(box)
     }
     let div = document.createElement('div')
     document.getElementsByClassName('atui-modal')[0].appendChild(div)
     new Vue({
-      el:div,
-      template:'<modal class="atui-modal" :show="show" small backdrop="false" :callback="callback"><div v-if="title" slot="modal-header"></div><div slot="modal-body" class="modal-body"><icon :type="help" color="#fa0"></icon>{{title}}<br/>{{content}}</div></modal>',
-      components:{
-        Modal:Modal
+      el: div,
+      template: '<modal class="atui-modal" :show="show" small backdrop="false" :callback="callback"><div v-if="title" slot="modal-header"></div><div slot="modal-body" class="modal-body"><icon :type="type" size="24" :color="color"></icon>{{title}}<br/>{{content}}</div><div v-if="type!==\'help\'" slot="modal-footer" class="modal-footer"><button type="button" class="btn btn-default" @click="this.show=false">知道了</button></div></modal>',
+      components: {
+        Modal: Modal,
+        Icon:Icon
       },
-      data:{
-        title:title,
-        content:content,
-        show:true
+      data: {
+        title: title,
+        content: content,
+        type:iconConfig[type].icon,
+        color:iconConfig[type].color,
+        show: true
       },
-      ready:function() {
+      ready: function() {
         let me = this
       },
-      methods:{
-        callback:function() {
+      methods: {
+        callback: function() {
           onOk()
           this.show = false
         }
       }
     })
-}
-
+  }
+})
 export default Modal
