cd5329f82c1dea84231ac13a881ab08817df2f4b
英布 <chris.chencq@alibaba-inc.com>
2016-04-23 22:39:58 +0800

    uglify打包错误解决

--- webpack.build.js
+++ webpack.build.js
@@ -2,52 +2,44 @@ var webpack = require('webpack')
 var config = require('./webpack.config.js')
 // delete config.devtool
 
-if (process.env.NODE_ENV !== 'production') {
-  config.entry = {
-      Vue: ['vue'],
-      VueComponent: ['./src/index.js'],
-      // VueWidget: ['./src/widgets/index.js'],
-      VueFilter: ['./src/filters/index.js']
-  }
-  config.output = {
-      path: './dist',
-      filename: '[name].dev.js',
-      library: '[name]',
-      libraryTarget: 'umd'
-  }
-  config.plugins = (config.plugins || []).concat([
-      new webpack.ProvidePlugin({
-          Vue: 'vue',
-      }),
-      // new webpack.ProvidePlugin({
-      //     VueComponent: 'vue-component'
-      // }),
-      new webpack.optimize.CommonsChunkPlugin({
-          name: "Vue",
-          filename: "vue.dev.js",
-      }),
-      //   new webpack.optimize.CommonsChunkPlugin({
-      // name: "VueComponent",
-      // filename: "vuecomponent.js",
-      //   }),
-      
-  ])
-  
-} else {
-  
-  config.entry = {
-      Vue: ['vue.dev.js'],
-      VueComponent: ['./dist/VueComponent.dev.js'],
-      // VueWidget: ['./dist/VueWidget.dev.js'],
-      VueFilter: ['./dist/VueFilter.dev.js']
-  }
+config.entry = {
+    Vue: ['vue'],
+    VueComponent: ['./src/index.js'],
+    // VueWidget: ['./src/widgets/index.js'],
+    VueFilter: ['./src/filters/index.js']
+}
+config.output = {
+    path: './dist',
+    filename: '[name].dev.js',
+    library: '[name]',
+    libraryTarget: 'umd'
+}
+config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+        Vue: 'vue',
+    }),
+    // new webpack.ProvidePlugin({
+    //     VueComponent: 'vue-component'
+    // }),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: "Vue",
+        filename: "vue.dev.js",
+    }),
+    //   new webpack.optimize.CommonsChunkPlugin({
+    // name: "VueComponent",
+    // filename: "vuecomponent.js",
+    //   }),
+    
+])
+
+if (process.env.NODE_ENV === 'production') {
   config.output = {
       path: './dist',
       filename: '[name].js',
       library: '[name]',
       libraryTarget: 'umd'
   }
-  config.plugins = [
+  config.plugins = config.plugins.concat([
     new webpack.DefinePlugin({
         'process.env': {
             NODE_ENV: '"production"'
@@ -62,8 +54,7 @@ if (process.env.NODE_ENV !== 'production') {
         }
     }),
     new webpack.optimize.OccurenceOrderPlugin()
-  ]
+  ])
 }
-
 
 module.exports = config
