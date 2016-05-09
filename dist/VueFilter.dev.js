c078b4787bf37c78e1df9b591d18d3fdda2caa69
英布 <chris.chencq@alibaba-inc.com>
2016-05-09 13:35:27 +0800

    修复ie9下的问题

--- dist/VueFilter.dev.js
+++ dist/VueFilter.dev.js
@@ -13,12 +13,12 @@ return webpackJsonp_name_([2],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(330);
+	module.exports = __webpack_require__(328);
 
 
 /***/ },
 
-/***/ 326:
+/***/ 324:
 /***/ function(module, exports) {
 
 	'use strict';
@@ -46,12 +46,12 @@ return webpackJsonp_name_([2],{
 
 /***/ },
 
-/***/ 330:
+/***/ 328:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
-	var _phoneNumber = __webpack_require__(326);
+	var _phoneNumber = __webpack_require__(324);
 	
 	var _phoneNumber2 = _interopRequireDefault(_phoneNumber);
 	
