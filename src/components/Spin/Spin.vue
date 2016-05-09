437300130b3039ea52ae5aa74c927a470bec98c4
英布 <chaoren1641@gmail.com>
2016-05-09 10:38:49 +0800

    ie9下动画支持侦测

--- src/components/Spin/Spin.vue
+++ src/components/Spin/Spin.vue
@@ -1,17 +1,18 @@
 <template>
 <div :class="['atui-spin',size,{sping:sping}]" v-show="show">
-  <div class="atui-sping-point">
-    <div></div>
-    <div></div>
-    <div></div>
+  <div v-if="isSupportAnimation" class="atui-sping-point">
+      <div></div>
+      <div></div>
+      <div></div>
   </div>
+  <p v-else class="atui-sping-text">加载中...</p>
   <div class="sping-content">
     <slot></slot>
   </div>
-
 </div>
 </template>
 <script>
+import isSupportAnimation from '../utils/cssAnimationSupported'
 export default {
   name: 'spin',
   props:{
@@ -26,6 +27,11 @@ export default {
       default:'default'
     }
   },
+  data() {
+    return {
+      isSupportAnimation:isSupportAnimation
+    }
+  },
   methods: {
     show () {
       this.show = true
@@ -33,8 +39,5 @@ export default {
     hide () {
       this.show = false
     }
-  },
-  created () {
-
   }
 }
