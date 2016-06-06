726c2ea673641df8dbb370382e50ab580a2a2e63
英布 <chris.chencq@alibaba-inc.com>
2016-06-06 11:33:19 +0800

    修复dropdown没有Icon问题

--- docs/example/dropdownDocs.vue
+++ docs/example/dropdownDocs.vue
@@ -4,8 +4,7 @@
     <div class="bs-example">
       <dropdown>
         <div data-toggle="dropdown">
-          点击显示菜单
-          <span class="caret"></span>
+          点击显示菜单 <icon type="down"></icon>
         </div>
         <ul name="dropdown-menu" class="dropdown-menu">
           <li><a>选项一</a></li>
@@ -16,8 +15,8 @@
         </ul>
       </dropdown>
       <dropdown>
-        <button type="button" class="btn btn-success" data-toggle="dropdown">
-          点击显示菜单 <span class="caret"></span>
+        <button type="button" class="btn" data-toggle="dropdown">
+          点击显示菜单 <icon type="down"></icon>
         </button>
         <ul name="dropdown-menu" class="dropdown-menu">
           <li><a>选项一</a></li>
@@ -29,7 +28,7 @@
       </dropdown>
       <dropdown>
         <button type="button" class="btn btn-primary" data-toggle="dropdown">
-          点击显示菜单 <span class="caret"></span>
+          点击显示菜单 <icon type="down"></icon>
         </button>
         <ul name="dropdown-menu" class="dropdown-menu">
           <li><a href="#">选项一</a></li>
@@ -41,7 +40,7 @@
       </dropdown>
       <dropdown trigger="hover">
         <a href="#" data-toggle="dropdown">
-          鼠标移上来显示 <span class="caret"></span>
+          鼠标移上来显示 <icon type="down"></icon>
         </a>
         <ul name="dropdown-menu" class="dropdown-menu">
           <li><a href="#">选项一</a></li>
@@ -54,10 +53,45 @@
     </div>
     <pre><code class="language-markup"><script type="language-mark-up">
 <dropdown>
-  <button type="button" class="btn btn-default" data-toggle="dropdown">
-    点击显示菜单
-    <span class="caret"></span>
+  <div data-toggle="dropdown">
+    点击显示菜单 <icon type="down"></icon>
+  </div>
+  <ul name="dropdown-menu" class="dropdown-menu">
+    <li><a>选项一</a></li>
+    <li><a>选项二</a></li>
+    <li><a>选项三</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">其它</a></li>
+  </ul>
+</dropdown>
+<dropdown>
+  <button type="button" class="btn" data-toggle="dropdown">
+    点击显示菜单 <icon type="down"></icon>
   </button>
+  <ul name="dropdown-menu" class="dropdown-menu">
+    <li><a>选项一</a></li>
+    <li><a>选项二</a></li>
+    <li><a>选项三</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">其它</a></li>
+  </ul>
+</dropdown>
+<dropdown>
+  <button type="button" class="btn btn-primary" data-toggle="dropdown">
+    点击显示菜单 <icon type="down"></icon>
+  </button>
+  <ul name="dropdown-menu" class="dropdown-menu">
+    <li><a href="#">选项一</a></li>
+    <li><a href="#">选项二</a></li>
+    <li><a href="#">选项三</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">其它</a></li>
+  </ul>
+</dropdown>
+<dropdown trigger="hover">
+  <a href="#" data-toggle="dropdown">
+    鼠标移上来显示 <icon type="down"></icon>
+  </a>
   <ul name="dropdown-menu" class="dropdown-menu">
     <li><a href="#">选项一</a></li>
     <li><a href="#">选项二</a></li>
@@ -92,10 +126,11 @@
 </template>
 
 <script>
-  import {Dropdown} from 'src/'
+  import {Dropdown,Icon} from 'src/'
   export default {
     components: {
-      Dropdown
+      Dropdown,
+      Icon
     }
   }
 </script>
