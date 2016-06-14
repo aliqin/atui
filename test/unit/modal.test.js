f100c7ae6b5686a67de35c2fbce97f396e4ff33a
国士 <jiangyi.jy@alibaba-inc.com>
2016-06-14 16:35:44 +0800

    完善modal组件单元测试用例

--- test/unit/modal.test.js
+++ test/unit/modal.test.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Modal from '../../src/components/Modal/'
 
-new Vue({
-  data () {
-    return {
-      show: true
-    }
-  },
-  template: `
+describe('Modal', () => {
+  let vm = new Vue({
+    data () {
+      return {
+        show: true
+      }
+    },
+    template: `
       <div>
         <modal title="Zoom Modal" :show.sync="show" effect="zoom" width="400px">
           <div slot="modal-header"></div>
@@ -15,17 +16,35 @@ new Vue({
             高圆圆，中国女演员，1979年10月5日出生于北京市丰台区云岗一个普通的知识分子家庭。1996年高圆圆被广告公司发掘，随后拍摄了大量广告，成为了广告圈中的模特。1997年高圆圆出演了她的第一部电影《爱情麻辣烫》，从此开始了她的演员生涯。2001年高圆圆参演的电影《十七岁的单车》获得柏林国际电影节最佳影片银熊奖。
           </div>
         </modal>
+        <modal title="modal2" :show.sync="show" width="400px" backdrop>
+          <div slot="modal-header"></div>
+          <div slot="modal-body" class="modal-body">
+            This is modal2 body content
+          </div>
+        </modal>
       </div>
       `,
-  components: { Modal }
-}).$mount().$appendTo('body')
-
-describe('Modal', () => {
+    components: {Modal}
+  }).$mount().$appendTo('body')
   it('Modal组件是否正常显示', () => {
-    expect(document.body.querySelectorAll('.atui-modal').length).above(0)
+    expect(document.body.querySelectorAll('.atui-modal').length).to.equal(2)
   })
 
-  it('事件回调', () => {
+  it('close方法调用', () => {
+    vm.$children[0].show = true
+    vm.$children[0].close()
+    expect(vm.$children[0].show).to.false
+  })
 
+  it('clickBack方法调用', () => {
+    // For backdrop prop set
+    vm.$children[1].show = true
+    vm.$children[1].clickBack()
+    expect(vm.$children[1].show).to.false
+
+    // For backdrop prop not set
+    vm.$children[0].show = true
+    vm.$children[0].clickBack()
+    expect(vm.$children[0].show).to.true
   })
 })
