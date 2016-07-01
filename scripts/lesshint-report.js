46120ca3a93022d6b34781d45260ea661d0a4bee
戊子 <xianqian.rxq@alibaba-inc.com>
2016-07-01 16:39:30 +0800

    lesshint检查已添加，已修复或者屏蔽error，warning待处理

---
+++ scripts/lesshint-report.js
@@ -0,0 +1,11 @@
+var originReport = require('lesshint/lib/reporters/stylish').report;
+
+module.exports = {
+  report: function (errors) {
+    // Use full path
+    errors.forEach(function(error) {
+      error.file = error.fullPath;
+    });
+    originReport(errors);
+  }
+};
