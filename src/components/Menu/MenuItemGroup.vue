f01102f4d539d08a273538433566e4af962f8eb6
英布 <chris.chencq@alibaba-inc.com>
2016-07-04 16:07:24 +0800

    menu组件完善，删除不必要的样式

---
+++ src/components/Menu/MenuItemGroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <li class="atui-menu-item-group">
+    <div class="atui-menu-submenu-title"></div>
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
