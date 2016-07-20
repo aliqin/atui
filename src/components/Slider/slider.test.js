2e0ba2330dd56ac8cb3f7b9324292be34316141a
国士 <jiangyi.jy@alibaba-inc.com>
2016-07-20 15:27:46 +0800

    增加slider组件单元测试

---
+++ src/components/Slider/slider.test.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jiangyi on 16/7/20.
+ */
+
+import Vue from 'vue'
+import Slider from '../Slider'
+
+describe('Slider组件单元测试', () => {
+  let vm = new Vue({
+    components: {
+      Slider
+    },
+    replace: false,
+    template: `
+      <slider :value="30"></slider>
+    `
+  }).$mount()
+
+  it('基础功能渲染', () => {
+    expect(vm.$el.querySelectorAll('.atui-slider').length).to.be.equal(1)
+    expect(vm.$el.querySelectorAll('.atui-slider-step').length).to.be.equal(1)
+    expect(vm.$el.querySelectorAll('.atui-slider-mark').length).to.be.equal(1)
+  })
+})
