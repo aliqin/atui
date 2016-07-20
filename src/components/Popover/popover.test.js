03008ede1f8222f540e3514488f26613e7623f0a
国士 <jiangyi.jy@alibaba-inc.com>
2016-07-20 14:59:48 +0800

    增加popover组件单元测试

---
+++ src/components/Popover/popover.test.js
@@ -0,0 +1,26 @@
+/**
+ * Created by jiangyi on 16/7/20.
+ */
+
+import Vue from 'vue'
+import Popover from '../Popover'
+
+describe('Popover组件单元测试', () => {
+  let vm = new Vue({
+    components: {
+      Popover
+    },
+    replace: false,
+    template: `
+      <popover effect="scale" title="this is title" content="this is content" placement="top" trigger="hover">
+        <button class="atui-btn">Hover</button>
+      </popover>
+    `
+  }).$mount()
+
+  it('基本渲染功能', () => {
+    expect(vm.$el.querySelectorAll('.atui-popover-trigger').length).to.be.equal(1)
+    expect(vm.$el.querySelectorAll('.atui-btn').length).to.be.equal(1)
+    expect(vm.$el.querySelectorAll('.atui-popover-top').length).to.be.equal(1)
+  })
+})
