e36bede36b4fb96693c78c7505d9a528114aa76e
戊子 <xianqian.rxq@alibaba-inc.com>
2016-07-21 15:29:39 +0800

    重新修改坐标定义，在Tooltip中，topLeft是指popup坐标顶部，左对齐，其他依次类推

--- src/components/Tooltip/Tooltip.vue
+++ src/components/Tooltip/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="[prefixCls + '-popover-cont']">
-    <trigger :trigger="trigger" :effect="effect" :placement="placement" popup-cls="tooltip">
+    <trigger :trigger="trigger" :effect="effect" :placement="placement" :popup-cls="popupCls">
       <slot slot="trigger"></slot>
       <slot slot="popup" name="popup" role="tooltip">
         <div :class="[prefixCls + '-tooltip-arrow']"></div>
@@ -27,7 +27,9 @@
 
     props: {
       trigger: {
-        type: String
+        type: String,
+        default: 'hover'
+
       },
       effect: {
         type: String
@@ -36,7 +38,31 @@
         type: String,
         default: 'top'
       },
-      content: [String, Number]
+      content: [String, Number],
+      popupCls: {
+        type: String,
+        default: 'tooltip'
+      }
+    },
+
+    events: {
+      'trigger-reset-pos': function (data) {
+        const { prefixCls, popupCls } = this
+        const { $trigger, $popup, placement } = data
+        const $arrow = $popup.querySelector(`.${prefixCls}-${popupCls}-arrow`)
+        const triggerOffset = $trigger.getBoundingClientRect()
+
+        // 修正箭头坐标
+        if (placement.endsWith('Left')) {
+          $arrow.style.left = `${triggerOffset.width / 2}px`
+        } else if (placement.endsWith('Right')) {
+          $arrow.style.right = `${triggerOffset.width / 2}px`
+        } else if (placement.endsWith('Top')) {
+          $arrow.style.top = `${triggerOffset.height / 2}px`
+        } else if (placement.endsWith('Bottom')) {
+          $arrow.style.bottom = `${triggerOffset.height / 2}px`
+        }
+      }
     }
   }
 </script>
