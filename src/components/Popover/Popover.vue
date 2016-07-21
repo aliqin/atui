accb62fbb2e87529aeec872c7e7d8565410600c2
戊子 <xianqian.rxq@alibaba-inc.com>
2016-07-21 11:35:57 +0800

    popover基于trigger统一组件改造

--- src/components/Popover/Popover.vue
+++ src/components/Popover/Popover.vue
@@ -1,24 +1,49 @@
 <template>
-  <div :class="[prefixCls + '-popover-trigger']" v-el:trigger>
-    <slot>
-    </slot>
-  </div>
-  <div v-el:popover
-       v-show="show"
-       :class="popoverClassObj"
-       :transition="effect">
-      <div :class="[prefixCls + '-popover-arrow']"></div>
-      <h3 :class="[prefixCls + '-popover-title']" v-show="title">{{title}}</h3>
-      <div :class="[prefixCls + '-popover-content']">
-        {{content}}
-      </div>
+  <div :class="[prefixCls + '-popover-cont']">
+    <trigger :trigger="trigger" :effect="effect" :placement="placement" popup-cls="popover">
+      <slot slot="trigger"></slot>
+      <slot slot="popup" name="popup">
+        <div :class="[prefixCls + '-popover-arrow']"></div>
+        <h3 :class="[prefixCls + '-popover-title']" v-show="header && title">{{title}}</h3>
+        <div :class="[prefixCls + '-popover-content']">
+          {{content}}
+        </div>
+      </slot>
+    </trigger>
   </div>
 </template>
 
 <script>
-import PopoverMixin from '../popoverMixins.js'
+  import GlobalMixin from '../_utils/GlobalMixin'
+  import Trigger from '../Trigger'
 
-export default {
-  mixins: [PopoverMixin]
-}
+  export default {
+    mixins: [
+      GlobalMixin
+    ],
+
+    components: {
+      Trigger
+    },
+
+    props: {
+      trigger: {
+        type: String
+      },
+      effect: {
+        type: String
+      },
+      placement: {
+        type: String
+      },
+      title: {
+        type: String
+      },
+      content: [String, Number],
+      header: {
+        type: Boolean,
+        default: true
+      }
+    }
+  }
 </script>
