a0b35f36b28b32c70cdb3fe9bd16dd18706400b3
弱冠 <qianshan.wengqs@alibaba-inc.com>
2016-07-22 10:26:11 +0800

    文案更新

--- site/theme/template/Home/Banner.jsx
+++ site/theme/template/Home/Banner.jsx
@@ -21,11 +21,11 @@ export default class Banner extends React.Component {
       <section id="banner">
         <ScrollElement scrollName="banner" className="page">
           <QueueAnim className="banner-text-wrapper" type={this.typeFunc} delay={300}>
-            <h2 key="h2">ANT <p>DESIGN</p></h2>
-            <p key="content">一个 UI 设计语言</p>
+            <h2 key="h2">Atui</h2>
+            <p key="content">阿里通信前端UI组件</p>
             <span className="line" key="line" />
             <div key="button" className="start-button clearfix">
-              <Link to="/docs/spec/introduce">
+              <Link to="/docs/react/introduce">
                 <Icon type="smile-circle" /> 开始探索
               </Link>
             </div>
