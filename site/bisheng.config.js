c77be2e289250398d67cd90920c9b7413d5059da
知春 <jianpeng.sjp@alibaba-inc.com>
2016-09-09 11:24:33 +0800

    谁把root改了？

--- site/bisheng.config.js
+++ site/bisheng.config.js
@@ -12,7 +12,7 @@ module.exports = {
   lazyLoad: true,
   theme: './site/theme',
   htmlTemplate: './site/theme/static/template.html',
-  root: '/bisheng-theme-one/',
+  root: '/',
   plugins: [
     'bisheng-plugin-description',
     'bisheng-plugin-toc?maxDepth=2',
