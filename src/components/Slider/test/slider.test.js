1048422b50bf2218432755467a77205d1a2eb15b
英布 <chris.chencq@alibaba-inc.com>
2017-01-22 17:08:35 +0800

    test:覆盖率检查，完善单元测试

--- src/components/Slider/test/slider.test.js
+++ src/components/Slider/test/slider.test.js
@@ -1,26 +1,28 @@
-/**
- * Created by jiangyi on 16/7/20.
- */
-
 import Vue from 'vue'
 import Slider from '../'
 
 describe('Slider组件单元测试', () => {
   let vm = new Vue({
     components: {
-      Slider
+      vSlider:Slider
     },
     replace: false,
     template: `
       <div>
-      <slider :value="30"></slider>
+      <v-slider :value="30"></v-slider>
+      <v-slider v-model="sliderValue"></v-slider>
+      <v-slider v-model="initRange"></v-slider>
       </div>
-    `
+    `,
+    data: {
+      sliderValue: 5,
+      initRange: [20,50]
+    }
   }).$mount()
 
   it('基础功能渲染', () => {
-    expect(vm.$el.querySelectorAll('.atui-slider').length).to.be.equal(1)
-    expect(vm.$el.querySelectorAll('.atui-slider-step').length).to.be.equal(1)
-    expect(vm.$el.querySelectorAll('.atui-slider-mark').length).to.be.equal(1)
+    expect(vm.$el.querySelectorAll('.atui-slider').length).to.be.equal(3)
+    expect(vm.$el.querySelectorAll('.atui-slider-step').length).to.be.equal(3)
+    expect(vm.$el.querySelectorAll('.atui-slider-mark').length).to.be.equal(3)
   })
 })
