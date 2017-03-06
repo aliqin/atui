67dbeaa6814bc0c904480953138e2b9d83041097
lynsun <814253326@qq.com>
2017-03-06 21:02:11 +0800

    feat: Affix组件支持target配置指定滚动的容器，位置相关的代码逻辑引用antd

---
+++ src/components/Affix/Affix.vue
@@ -0,0 +1,100 @@
+<template>
+<div>
+  <div v-bind:class="{'vue-affix': affixed}"
+    v-bind:style="styles">
+    <slot></slot>
+  </div>
+</div>
+</template>
+
+<script>
+import EventListener from '../_utils/EventListener.js'
+import getScroll from '../_utils/getScroll.js'
+
+export default {
+  name: 'Affix',
+  props: {
+    offset: {
+      type: Number,
+      default: 0
+    },
+    target: {
+      type: Function,
+      default: () => window
+    }
+  },
+  data () {
+    return {
+      affixed: false,
+      styles: {}
+    }
+  },
+  methods: {
+    scrolling () {
+      const target = this.target()
+      const scrollTop = getScroll(target, true)
+      const affixNode = this.$el
+      const elementOffset = this.getOffset(affixNode, target)
+      const targetRect = this.getTargetRect(target)
+      if (!this.affixed && scrollTop > elementOffset.top) {
+        this.affixed = true
+        this.styles = {
+          top: this.offset + targetRect.top + 'px',
+          left: elementOffset.left + targetRect.left + 'px',
+          width: this.$el.offsetWidth + 'px'
+        }
+      }
+      if (this.affixed && scrollTop < elementOffset.top) {
+        this.affixed = false
+        this.styles = {}
+      }
+    },
+    // reffered from antd
+    getTargetRect (target) {
+      return target !== window
+        ? target.getBoundingClientRect()
+        : { top: 0, left: 0, bottom: 0 }
+    },
+    // reffered from antd
+    getOffset (element, target) {
+      const elemRect = element.getBoundingClientRect()
+      const targetRect = this.getTargetRect(target)
+
+      const scrollTop = getScroll(target, true)
+      const scrollLeft = getScroll(target, false)
+
+      const docElem = window.document.body
+      const clientTop = docElem.clientTop || 0
+      const clientLeft = docElem.clientLeft || 0
+
+      return {
+        top: elemRect.top - targetRect.top +
+          scrollTop - clientTop,
+        left: elemRect.left - targetRect.left +
+          scrollLeft - clientLeft,
+        width: elemRect.width,
+        height: elemRect.height
+      }
+    }
+  },
+  mounted () {
+    let target = this.target()
+    this._scrollEvent = EventListener.listen(target, 'scroll', this.scrolling)
+    this._resizeEvent = EventListener.listen(target, 'resize', this.scrolling)
+  },
+  beforeDestroy () {
+    if (this._scrollEvent) {
+      this._scrollEvent.remove()
+    }
+    if (this._resizeEvent) {
+      this._resizeEvent.remove()
+    }
+  }
+}
+</script>
+
+<style>
+  .vue-affix {
+    position: fixed;
+  }
+</style>
