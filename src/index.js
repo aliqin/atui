41d565e396615e7ec05d11eb38d0c035def2bbe0
lynsun <814253326@qq.com>
2017-03-18 00:26:09 +0800

    chore: version change

--- src/index.js
+++ src/index.js
@@ -110,7 +110,7 @@ const VueComponent = {
 }
 
 module.exports = { ...VueComponent }
-module.exports.version = '0.0.34'
+module.exports.version = '0.0.36'
 
 module.exports.install = (Vue, opts) => {
   let prefix = opts && opts.prefix || 'v'
