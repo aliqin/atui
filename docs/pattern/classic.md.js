707131c70f4bafc5792e4aeaa555125749b1dc50
lynsun <814253326@qq.com>
2017-03-20 14:05:34 +0800

    Updates

--- docs/pattern/classic.md.js
+++ docs/pattern/classic.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([187,194],{349:function(e,s){"use strict";e.exports={content:["article",["p","\u656c\u8bf7\u671f\u5f85\u3002"]],meta:{order:0,disabled:!0,chinese:"\u5178\u578b\u9875\u9762",english:"Classic",filename:"docs/pattern/classic.md"},toc:["ul"]}}});
+webpackJsonp([187,194],{350:function(e,s){"use strict";e.exports={content:["article",["p","\u656c\u8bf7\u671f\u5f85\u3002"]],meta:{order:0,disabled:!0,chinese:"\u5178\u578b\u9875\u9762",english:"Classic",filename:"docs/pattern/classic.md"},toc:["ul"]}}});
