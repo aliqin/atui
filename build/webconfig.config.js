45452547558d0b4b9b02c82a5a27b40e2e8e0b60
英布 <chris.chencq@alibaba-inc.com>
2017-10-24 17:38:43 +0800

    feat:版本号更新

--- build/webconfig.config.js
+++ build/webconfig.config.js
@@ -9,8 +9,7 @@ var autoprefixer = require('autoprefixer')
 // var styleLintPlugin = require('stylelint-webpack-plugin')
 // var version = process.env.VERSION || require('./package.json').version
 var projectRoot = path.resolve(__dirname, 'src/')
-var git = require('git-rev-sync')
-var version = git.branch().replace('daily/','')
+const version = process.env.VERSION || require('../package.json').version
 
 var banner =
   '\n/*!\n' +
--- dist/atuiWidget.js
+++ dist/atuiWidget.js
@@ -1,6 +1,6 @@
 
 /*!
- * atui vdev
+ * atui v0.1.3
  * (c) 2017 alibaba
  * Released under the MIT License.
  */
